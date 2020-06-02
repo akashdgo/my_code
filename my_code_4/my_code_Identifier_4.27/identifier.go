46e88b4a55b6ba020ccd5c62db630289985f4102
akashdgo <akashd.go@yandex.ru>
2020-06-02 11:48:11 -0700

    Commit: Identifief program

---
+++ my_code_4/my_code_Identifier_4.27/identifier.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// All the lines below are each a statement
+	// Each statement takes up one line.
+
+	// This is declaring x : and assigning a value.
+	x := 42
+	fmt.Println(x)
+
+	// This is value assigned to the already declared varible.
+	x = 77
+	fmt.Println(x)
+
+	// The int '100' & '77' are Operands and '+' is the Operator
+	// Each statement is made up of one or more explicit values, constants, variables, operators, and functions
+	// the programming language interprets and computes to produce another value.
+
+	y := 100 + 77  // '100 +77' is an expression.
+	fmt.Println(y) // All declared variable need to be used.
+}
