f8f258123d6aa3f30c979bacaeb06043b76fb3c0
akashdgo <akashd.go@yandex.ru>
2020-06-07 19:38:30 -0700

    Commit iota.go

---
+++ my_code_6/6.46_iota/iota.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a = iota
+	b = iota
+	c = iota
+)
+const (
+	e = iota
+	f
+	g
+)
+
+const (
+	l = iota
+	m
+	n
+	o
+	p
+	q
+	r
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(l)
+	fmt.Println(m)
+	fmt.Println(n)
+	fmt.Println(o)
+	fmt.Println(p)
+	fmt.Println(q)
+	fmt.Println(r)
+}
